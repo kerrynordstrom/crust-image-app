cb7b4dbc456dbf263750e0e7aff61d738a3d127c
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2020-12-31 18:55:08 -0600

    :hammer: Handle root relative linking

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,5 +1,5 @@
-export const domain =
-  process.env.NODE_ENV &&
-  process.env.NODE_ENV === "production"
+export const domain = 
+  process.env.REACT_APP_ENVIRONMENT &&
+  process.env.REACT_APP_ENVIRONMENT === "production"
     ? process.env.REACT_APP_PROD_API_URL
     : process.env.REACT_APP_DEV_API_URL;
